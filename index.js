150e2cbd925cdc794a3a152eda9e4a8bb39a8d8e
Sydney Michael <m13s19madden@gmail.com>
2020-03-11 20:33:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ function nowServing(line) {
     return 'There is nobody waiting to be served!'
   }
   else {
-    return line[0], line.shift()
+    return `Currently serving ${line[0]}.`, line.shift()
   }
 }
