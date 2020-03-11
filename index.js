37d2a5619cb6b1a1cdc7fbaa6ccb4fafdc04f75d
Sydney Michael <m13s19madden@gmail.com>
2020-03-11 20:59:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function currentLine(line){
   if (line.length === 0){
     return 'The line is currently empty.'
   } else {
-    st = 'The line is currently:'
+    var st = 'The line is currently:'
     let i = 1
     do {
       st += ` ${i + 1}. ${name},`
