612f2c57c149703dc2344d052b379ce59149a577
Sydney Michael <m13s19madden@gmail.com>
2020-03-11 20:30:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function takeANumber(line, name) {
   let i = 0
-  while (line[i] != name && i < line.length) {
+  while (line[i] != name && i < line.length + 1) {
     i++
   }
   return `Welcome, ${name}. You are number ${i + 1} in line.`
